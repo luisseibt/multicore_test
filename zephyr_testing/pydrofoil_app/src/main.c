90c9e8b5c39881f2a6c7b7f08687dd8551cbade7
Luis Seibt <seibt@ice.rwth-aachen.de>
2026-03-24 20:09:25 +0100

    multiprocessing working with pydrofoil

--- zephyr_testing/pydrofoil_app/src/main.c
+++ zephyr_testing/pydrofoil_app/src/main.c
@@ -1,21 +1,48 @@
-#include <stdint.h>
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
 
-/* Use 'used' and ensure they are global (no static) */
-__attribute__((used, section(".data"), aligned(16)))
-volatile uint64_t tohost = 0;
+#define STACK_SIZE 1024
+#define THREAD_PRIORITY 7
 
-__attribute__((used, section(".data"), aligned(16)))
-volatile uint64_t fromhost = 0;
+/* 1. We must manually allocate memory for the new thread's stack */
+K_THREAD_STACK_DEFINE(my_stack_area, STACK_SIZE);
+struct k_thread my_thread_data;
 
+/* 2. The function our second thread will run */
+void secondary_thread_function(void *arg1, void *arg2, void *arg3)
+{
+    for(int i = 0; i < 10; i++) {
+        printk("1\n");
+        k_busy_wait(100);
+    }
+}
+
+/* 3. The Main function (Core 0) */
 int main(void)
 {
-    /* FAKE USE: This prevents the linker from garbage collecting the symbols */
-    if (tohost > 0) {
-        printf("This will never happen, but the linker doesn't know that: %llu", tohost);
+    printk("Starting Multicore SMP Test...\n");
+
+    /* THIS IS WHAT YOU EXPECTED: 
+     * Explicitly telling the OS to create and start the second thread! 
+     */
+    k_thread_create(&my_thread_data, my_stack_area,
+                    K_THREAD_STACK_SIZEOF(my_stack_area),
+                    secondary_thread_function,
+                    NULL, NULL, NULL,
+                    THREAD_PRIORITY, 0, K_NO_WAIT);
+
+    /* Main thread loop */
+    for(int i = 0; i < 10; i++) {
+        printk("0\n");
+        k_busy_wait(100);
     }
-
-    printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
     return 0;
 }
+
+
+
+
+
+
+
+
